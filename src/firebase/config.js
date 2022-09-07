605534a8374fbe5a7b06ccc21cdd90f59e349486
Stefano Consonni <stefanoconsonni@ymail.com>
2022-09-07 12:49:27 +0200

    setup firebase-firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCG3J4oQ2Gd92XMmPuP1UYnnoGvi7YG0gU",
+	authDomain: "money-tracker-3f8f5.firebaseapp.com",
+	projectId: "money-tracker-3f8f5",
+	storageBucket: "money-tracker-3f8f5.appspot.com",
+	messagingSenderId: "33892283927",
+	appId: "1:33892283927:web:90655fe6aac5e9b8e70e52",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
