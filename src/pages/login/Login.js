60fdf748c3255d6301eff74e3e56b01f65f1259e
Stefano Consonni <stefanoconsonni@ymail.com>
2022-09-07 12:23:27 +0200

    add form to Login.js

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
 // styles
 import styles from "./Login.module.css";
 
 export default function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(email, password);
+	};
+
 	return (
-		<div>
-			<h1>Login</h1>
-		</div>
+		<form className={styles["login-form"]} onSubmit={handleSubmit}>
+			<h2>Login</h2>
+			<label>
+				<span>email:</span>
+				<input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+			</label>
+
+			<label>
+				<span>password:</span>
+				<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+			</label>
+			<button className="btn">Login</button>
+		</form>
 	);
 }
